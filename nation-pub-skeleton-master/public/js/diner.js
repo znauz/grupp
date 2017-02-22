ae50369b30d82e681dc3abef3e79afa84b803adf
Karabom <sebastian.lindgren1994@gmail.com>
2017-02-23 00:46:02 +0100

    Updated Functionality on several aspects

--- nation-pub-skeleton-master/public/js/diner.js
+++ nation-pub-skeleton-master/public/js/diner.js
@@ -1,6 +1,8 @@
 /* global sharedVueStuff, Vue, socket */
 'use strict';
 
+var oNR = 0;
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -11,10 +13,9 @@ function getOrderNumber() {
     // It's probably not a good idea to generate a random order
     //  number, client-side. 
     // A better idea would be to let the server decide.
-    return "#" + getRandomInt(1, 1000000);
+    oNR++;
+    return "#" + oNR;
 }
-
-
 
 new Vue({
     el: '#ordering',
@@ -25,45 +26,54 @@ new Vue({
         mainDish: [],
         extras: [],
         extrasInfo: [],
-        total: 0
-
+        total: 0,
+        calc: 0,
     },
     methods: {
         placeOrder: function() {
-            var arr = document.getElementsByClassName('ordersInProgress');
-            var i=arr.length-1;
-            for(i; i>-1; i--) {
-                arr[i].parentNode.removeChild(arr[i]);
-            }
-            this.total = 0;
-            // Here two ways of getting selected items are illustrated
-            // 1. The Vue way, notice the data model declarations
-            //  above
-            
-            //var mainCourse = this.mainDish;
-            //var extras = this.extras;
-            // 2. The old-school way: create an array with values of
-            //  checked items
-            //var theRest = [].filter.call(document.getElementsByName('item[]'), function(i) {
-              //  return i.checked;
-            //}).map(function(i) {
-              //  return i.value;
-            //});
-            // OK, it's not really neat to use two different ways
-            //  of accomplishing the same thing
-            // but let's pretend it's for an educational purpose ...
-            //  here comes another no-no:
-            //var orderItems = mainCourse.concat(extras).concat(theRest);
-            // Finally we make use of socket.io's magic to send the
-            //  stuff to the kitchen
+            if(this.toOrder.length > 0) {
+                var arr = document.getElementsByClassName('ordersInProgress');
+                var i=arr.length-1;
+                for(i; i>-1; i--) {
+                    arr[i].parentNode.removeChild(arr[i]);
+                }
+                this.total = 0;
+                // Here two ways of getting selected items are illustrated
+                // 1. The Vue way, notice the data model declarations
+                //  above
+                
+                //var mainCourse = this.mainDish;
+                //var extras = this.extras;
+                // 2. The old-school way: create an array with values of
+                //  checked items
+                //var theRest = [].filter.call(document.getElementsByName
+                // ('item[]'), function(i) {
+                //  return i.checked;
+                //}).map(function(i) {
+                //  return i.value;
+                //});
+                // OK, it's not really neat to use two different ways
+                //  of accomplishing the same thing
+                // but let's pretend it's for an educational purpose ...
+                //  here comes another no-no:
+                //var orderItems = mainCourse.concat(extras).concat(theRest);
+                // Finally we make use of socket.io's magic to send the
+                //  stuff to the kitchen
 
-            var orderItems = [];
-            var i=0;
-            for(i; i<this.toOrder.length; i++) {
-                orderItems.push(this.toOrder[i].label);
+                var orderItems = [];
+                var i=0;
+                for(i; i<this.toOrder.length; i++) {
+                    console.log(this.toOrder[i].thing);
+                    console.log(this.toOrder[i].label);
+                    if (this.toOrder[i].thing != "drink") {
+                        orderItems.push(this.toOrder[i].label);
+                    }
+                }
+
+                socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
+                this.toOrder = [];
+                this.orderNumber++;
             }
-            
-            socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
         },
         sendToReceipt: function(item) {
             var orderTable = document.getElementById('currentOrder');
@@ -121,6 +131,71 @@ new Vue({
                 var newArr = {id: id, name: name, price: price};
                 this.extrasInfo.push(newArr);
             }
+        },
+        // When the user clicks on the button, open the modal
+        openModal: function() {
+            // Get the modal
+            this.calc = 0;
+            var text = document.getElementById('calcInfo');
+            text.value = this.calc;
+            
+            var modal = document.getElementById('myModal');
+            
+            modal.style.display = "block";
+        },
+        openModal2: function() {
+            // Get the modal
+            var modal = document.getElementById('myModal2');
+
+            modal.style.display = "block";
+        },
+        // When the user clicks on <span> (x), close the modal
+        closeModal: function() {
+            // Get the modal
+            var orgTot = this.total;
+            var calcVal = this.calc;
+            var currTot = 0;
+
+            if (orgTot != 0) {
+                this.total -= calcVal;
+                currTot = this.total;
+                this.total = 0;
+                this.calcVal = 0;
+            }
+            
+            var modal = document.getElementById('myModal');
+            var modal2 = document.getElementById('myModal2');
+            
+            modal.style.display = "none";
+            modal2.style.display = "none";
+
+            if (orgTot != 0) {
+                if ((currTot === 0) || (currTot < 0)) {
+                    if (currTot < 0) {
+                        window.alert("return: " + currTot*(-1) + " kr");
+                        this.total = 0;
+                    }
+                    this.placeOrder();
+                }
+            }
+        },
+        addNum: function(value) {
+            var info = document.getElementById('calcInfo');
+
+            var value = this.calc = this.calc*10 + value;
+            info.value = value;
+        },
+        clear: function(value) {
+            var info = document.getElementById('calcInfo');
+
+            var value = this.calc = 0;
+            info.value = value;
+        },
+        back: function() {
+            var info = document.getElementById('calcInfo');
+
+            var value = this.calc = (this.calc - (this.calc%10))/10;
+            info.value = value;
         }
     }
 });
@@ -146,4 +221,17 @@ function openFood(evt, food) {
     //  that opened the tab
     document.getElementById(food).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+window.onclick = function(event) {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    var modal2 = document.getElementById('myModal2');
+    
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
 }
